7236fdcc97eba2103c84bfc97ed9fab8f593cf92
ChanBong <harsh_k@ma.iitr.ac.in>
2023-01-28 00:18:19 +0530

    hgs endpoint created and tested

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
+import json
 from models import HGS_Solver
 app = Flask(__name__)
 
-# import data from data/toy_data.json
-import json
-with open('data/toy_data.json') as json_file:
-    data = json.load(json_file)
-
-
-@app.route('/')
-def algorithm():
-
-    # Solver initialization
-    hgs_solver = HGS_Solver(data, timeLimit=3.2)
-
-    # Solve
+@app.route('/hgs', methods=['POST'])
+def hgs():
+    """
+    HGS Solver:
+    uses the vanilla HGS solver to solve the CVRP
+    """
+    data = request.get_json()
+    hgs_solver = HGS_Solver(data)
     result = hgs_solver.solve_cvrp(data)
-    print(result.cost)
-    print(result.routes)
-
     return jsonify(
     {
         "cost": result.cost,
         "routes": result.routes
     })
 
-app.run(host='0.0.0.0', port=9876)
+@app.route('/oml', methods=['POST'])
+def oml():
+    data = request.get_json()
+    result = HGS_Solver.solve(data)
+    return jsonify(result)
+
+# defalut landing page
+@app.route('/')
+def index():
+    return "Welcome to the HGS API" 
+
+
+if __name__ == '__main__':    
+    app.run(host='0.0.0.0', port=9876)
+
--- models.py
+++ models.py
@@ -1,12 +1,28 @@
 import numpy as np 
 import hygese as hgs 
+from collections import OrderedDict
+from tools import write_vrplib, read_vrplib
+import json
 
 class HGS_Solver():
-    def __init__(self, data, timeLimit=3.2):
-        self.timeLimit = timeLimit
+    def __init__(self, data):
+        self.input_data = data
+        self.instance_name = data['instance_name']
+        self.instance = read_vrplib(self.instance_name)
+        # print(self.instance)
+
+        hgs_data = OrderedDict()
+        hgs_data['distance_matrix'] = self.instance['duration_matrix']
+        hgs_data['num_vehicles'] = self.instance['vehicle_count']
+        hgs_data['depot'] = 0
+        hgs_data['demands'] = self.instance['demands']
+        hgs_data['vehicle_capacity'] = self.instance['capacity']
+        hgs_data['service_times'] = self.instance['service_times']
+        
+        self.data = hgs_data
+        self.timeLimit = data['epoch_tlim']
         self.ap = hgs.AlgorithmParameters(timeLimit=self.timeLimit)
         self.hgs_solver = hgs.Solver(parameters=self.ap, verbose=True)
-        self.data = data
 
     def solve_cvrp(self, data):
         result = self.hgs_solver.solve_cvrp(self.data)
